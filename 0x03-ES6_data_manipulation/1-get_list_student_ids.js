1eb50bc383e738d1602979204cd33d95cfc3dc2b
Yahweh's Delight <intricatesyllable@gmail.com>
2024-10-09 01:55:22 +0300

    Add: Function takes in an array of objects^Ceturns an array of ids from a list of object.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,18 +5,3 @@ export default function getListStudentIds(param) {
     }
     return res;
 }
-
-
-
-
-
-
-// Given an list of object, return only the array of ids
-
-// Create a function getListStudentIds that returns an array of ids from a list of object./
-
-// This function is taking one argument which is an array of objects - and this array is the same format as getListStudents from the previous task.
-
-// If the argument is not an array, the function is returning an empty array.
-
-// You must use the map function on the array.
