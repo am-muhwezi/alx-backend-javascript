f50928bff146a4a51914a0d4b646493cbd38da33
Yahweh's Delight <michealphenh10@gmail.com>
2025-01-21 14:36:45 +0300

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,15 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
 export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-	return [];
-    }
-    return students.map(student => student.id);
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
