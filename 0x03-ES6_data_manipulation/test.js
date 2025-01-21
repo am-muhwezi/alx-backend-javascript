1fcc72a70982ccdf046e8673358928f44e82e1d4
Yahweh's Delight <michealphenh10@gmail.com>
2025-01-21 14:20:33 +0300

    data structs in ECMA 6

---
+++ 0x03-ES6_data_manipulation/test.js
@@ -0,0 +1,7 @@
+const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+];
+const ids = students.map(ids => ids.id);
+console.log(ids);
